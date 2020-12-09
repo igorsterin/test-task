994df4977fa833249f3a9a93e16834c1e12c531e
игорь стерин <igor234xb@gmail.com>
2020-12-10 00:16:06 +0700

    model editForm.php v1.2 (совместима с viev edit.php v1.5)

--- models/EditForm.php
+++ models/EditForm.php
@@ -14,12 +14,16 @@ class EditForm extends Model
     public $lastname;
     public $middlename;
     public $birthdate;
+    public $city;
     public $mobile;
     public $sex;
     public $salary;
     public $employment;
     public $shedule;
     public $aboutme;
+    public $photo;
+    public $specialization;
+    
     public $qqq;
     public $aaa;
         
@@ -39,13 +43,13 @@ class EditForm extends Model
         ]; */
         
       return [
-           [['lastname', 'name', 'middlename', 'email', 'mobile', 'birthdate', 'salary'], 'required', 'message' => 'Поле обязательно для заполнения'],
+           [['photo','lastname', 'name', 'middlename', 'email', 'mobile', 'birthdate', 'salary', 'specialization', 'city'], 'required', 'message' => 'Поле обязательно для заполнения'],
+           [['sex', 'employment', 'shedule', 'aboutme'], 'default', 'value' => null],
            ['email', 'email', 'message' => 'Поле заполнено неверно'],
-           ['email', 'email', 'message' => 'Поле заполнено неверно'],
-           ['salary', 'integer', 'min' => '1', 'tooSmall' => 'Поле заполнено неверно'], 
+           ['salary', 'integer', 'min' => '1', 'tooSmall' => 'Поле заполнено неверно', 'message' => 'Поле заполнено неверно'], 
            //['mobile', 'match', 'pattern' => '/^[+7]{1}[+|0-9| |-]*$/i', 'message' => 'Поле заполнено неверно'], 
           // ['mobile', 'match', 'pattern' => '/^[+7][+|0-9]{3}$/i', 'message' => 'Поле заполнено неверно'], 
-            ['mobile', 'match', 'pattern' => '/^(\+7)[ |-]?[0-9]{3}[ |-]?[0-9]{3}[ |-]?[0-9]{2}[ |-]?[0-9]{2}$/i', 'message' => 'Поле заполнено неверно'], //пропускает только номера +7 123 456 78 90, после 7 только 10 цифр, на месте пробелов так же могут быть тире, либо же пробелов может не быть.
+            ['mobile', 'match', 'pattern' => '/^(\+7)[ |-]?[0-9]{3}[ |-]?[0-9]{3}[ |-]?[0-9]{2}[ |-]?[0-9]{2}$/i', 'message' => 'Поле заполнено неверно'], //пропускает только номера по шаблону +7 123 456 78 90, после 7 только 10 цифр, на месте пробелов так же могут быть тире, либо же пробелов может не быть (слитное написание допускается).
       
         ]; 
     } 
